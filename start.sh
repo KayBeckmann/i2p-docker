17095b9efaace85a4d8b5aba38925032b2a6fb8a
Kay <info@beckmann-md.de>
2026-04-17 15:17:44 +0200

    Update start.sh: Add xhost permission for X11-forwarding

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# Startet die Standard-I2P-Umgebung mit X11-Forwarding
+# Erlaubt Docker den Zugriff auf den X-Server (für das GUI-Fenster)
+xhost +local:docker
+
+# Startet die Standard-I2P-Umgebung
 docker-compose up -d
